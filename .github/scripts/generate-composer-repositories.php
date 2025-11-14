2d771f296a2c7b9b47ccf2c7e084b63c9f9c462b
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-11-14 16:05:36 +0100

    wip

--- .github/scripts/generate-composer-repositories.php
+++ .github/scripts/generate-composer-repositories.php
@@ -1,14 +1,23 @@
 <?php
 
-$files = glob('packages/*', GLOB_ONLYDIR);
+$packagesDir = $argv[1] ?? 'packages';
+$files = glob($packagesDir . '/*', GLOB_ONLYDIR);
 
+$packageNames = [];
 $repositories = [];
 
 foreach($files as $file) {
-    $repositories[] = [
-        'type' => 'path',
-        'url' => ltrim($file, './'),
-    ];
+    $composerFile = $file . '/composer.json';
+    if (file_exists($composerFile)) {
+        $packageComposer = json_decode(file_get_contents($composerFile), true);
+        if (isset($packageComposer['name'])) {
+            $packageNames[] = $packageComposer['name'];
+            $repositories[] = [
+                'type' => 'path',
+                'url' => ltrim($file, './'),
+            ];
+        }
+    }
 }
 
 $composer = json_decode(file_get_contents('composer.json'), true);
@@ -19,3 +28,6 @@ file_put_contents(
     'composer.json',
     json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
 );
+
+// Output package names for use in workflow
+echo implode(' ', $packageNames);
