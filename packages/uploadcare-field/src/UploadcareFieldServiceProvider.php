57efa1084dc3162923922badc7007a5c29da08b3
Baspa <10845460+Baspa@users.noreply.github.com>
2025-11-14 10:37:54 +0000

    fix: styling

--- packages/uploadcare-field/src/UploadcareFieldServiceProvider.php
+++ packages/uploadcare-field/src/UploadcareFieldServiceProvider.php
@@ -24,14 +24,14 @@ class UploadcareFieldServiceProvider extends PackageServiceProvider
     public function packageBooted(): void
     {
         FilamentAsset::register([
-            Css::make('uploadcare-field', __DIR__.'/../resources/dist/uploadcare-field.css'),
-            Js::make('media-picker', __DIR__.'/../resources/js/media-picker.js'),
+            Css::make('uploadcare-field', __DIR__ . '/../resources/dist/uploadcare-field.css'),
+            Js::make('media-picker', __DIR__ . '/../resources/js/media-picker.js'),
         ], 'backstage/uploadcare-field');
     }
 
     public function bootingPackage(): void
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'backstage-uploadcare-field');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'backstage-uploadcare-field');
 
         // Register Livewire components
         $this->app->make('livewire')->component('backstage-uploadcare-field::media-grid-picker', \Backstage\UploadcareField\Livewire\MediaGridPicker::class);
