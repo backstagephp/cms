cee53cfb9cfb4f6e62310b65d8944756aaaf353e
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-11-14 16:15:00 +0100

    wip

--- .github/scripts/generate-composer-repositories.php
+++ .github/scripts/generate-composer-repositories.php
@@ -25,6 +25,7 @@ $composer = json_decode(file_get_contents('composer.json'), true);
 
 $composer['repositories'] = $repositories;
 
+
 file_put_contents(
     'composer.json',
     json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
